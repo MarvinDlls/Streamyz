c67ea09007f6365afa58acf53a92ef61c9450f7c
Marvindlls <marvindillenseger@gmail.com>
2025-02-17 12:47:02 +0100

    Refactor TmdbApiService to update popular movies endpoint and add searchMovies method

--- src/Service/TmdbApiService.php
+++ src/Service/TmdbApiService.php
@@ -9,18 +9,33 @@ class TmdbApiService
     private HttpClientInterface $client;
     private string $apiKey;
 
-    public function __construct(HttpClientInterface $client, string $apiKey)
-    {
+    public function __construct(
+        HttpClientInterface $client,
+        string $apiKey
+    ) {
         $this->client = $client;
         $this->apiKey = $apiKey;
     }
 
     public function fetchPopularMovies(): array
     {
-        $response = $this->client->request('GET', 'https://api.themoviedb.org/3/search/movie?&query=dragon', [
+        $response = $this->client->request('GET', 'https://api.themoviedb.org/3/movie/popular', [
             'query' => [
                 'api_key' => $this->apiKey,
                 'language' => 'fr-FR'
+            ]
+        ]);
+
+        return $response->toArray();
+    }
+
+    public function searchMovies(string $query): array
+    {
+        $response = $this->client->request('GET', 'https://api.themoviedb.org/3/search/movie', [
+            'query' => [
+                'api_key' => $this->apiKey,
+                'language' => 'fr-FR',
+                'query' => $query
             ]
         ]);
 
