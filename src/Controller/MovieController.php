dfe9e77bc8a5ea2f21816983d5613f5dc7377cb0
Marvindlls <marvindillenseger@gmail.com>
2025-02-17 12:46:57 +0100

    Add search functionality to MovieController and create player route

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\TmdbApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,12 +18,34 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movies', name: 'movie_list')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $movies = $this->tmdbApiService->fetchPopularMovies();
+        $search = $request->query->get('search');
+        
+        if ($search) {
+            $movies = $this->tmdbApiService->searchMovies($search);
+        } else {
+            $movies = $this->tmdbApiService->fetchPopularMovies();
+        }
 
         return $this->render('movies/index.html.twig', [
             'movies' => $movies['results'],
+            'search' => $search
+        ]);
+    }
+
+    
+    #[Route('/player', name: 'player')]
+    public function id(): Response
+    {
+        return $this->render('components/player.html.twig', [
+            'video_url' => 'chemin/vers/video.mp4',
+            'width' => '640',
+            'height' => '360',
+            'controls' => true,
+            'autoplay' => false,
+            'loop' => false,
+            'mime_type' => 'video/mp4'
         ]);
     }
 }
