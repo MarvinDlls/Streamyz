52ffca3b2bd6fc5f5bb2c816db172de0842efdb1
Marvindlls <marvindillenseger@gmail.com>
2025-02-18 16:34:37 +0100

    Add method to fetch all movies with pagination support

--- src/Service/TmdbApiService.php
+++ src/Service/TmdbApiService.php
@@ -29,17 +29,39 @@ class TmdbApiService
         return $response->toArray();
     }
 
+    public function fetchAllMovies(int $maxPages = 10): array
+    {
+        $allMovies = [];
+        $page = 1;
+
+        do {
+            $response = $this->client->request('GET', 'https://api.themoviedb.org/3/discover/movie', [
+                'query' => [
+                    'api_key' => $this->apiKey,
+                    'language' => 'fr-FR',
+                    'page' => $page
+                ]
+            ]);
+
+            $data = $response->toArray();
+            $allMovies = array_merge($allMovies, $data['results']);
+            $page++;
+        } while ($page <= $data['total_pages'] && $page <= $maxPages);
+
+        return $allMovies;
+    }
+
     public function fetchGenreMovies(string $genre): array
     {
-    $response = $this->client->request('GET', 'https://api.themoviedb.org/3/discover/movie', [
-        'query' => [
-            'api_key' => $this->apiKey,
-            'language' => 'fr-FR',
-            'with_genres' => $genre
-        ]
-    ]);
+        $response = $this->client->request('GET', 'https://api.themoviedb.org/3/discover/movie', [
+            'query' => [
+                'api_key' => $this->apiKey,
+                'language' => 'fr-FR',
+                'with_genres' => $genre
+            ]
+        ]);
 
-    return $response->toArray();
+        return $response->toArray();
     }
 
     public function fetchDetailMovie(int $id): array
@@ -52,30 +74,30 @@ class TmdbApiService
         ]);
 
         return $response->toArray();
-    }   
-    
+    }
+
     public function videoMovie(int $id): array
     {
-    $response = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/videos", [
-        'query' => [
-            'api_key' => $this->apiKey,
-            'language' => 'fr-FR'
-        ]
-    ]);
+        $response = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/videos", [
+            'query' => [
+                'api_key' => $this->apiKey,
+                'language' => 'fr-FR'
+            ]
+        ]);
 
-    return $response->toArray();
+        return $response->toArray();
     }
 
     public function reviewMovie(int $id): array
     {
-    $response = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/reviews", [
-        'query' => [
-            'api_key' => $this->apiKey,
-            'language' => 'fr-FR'
-        ]
-    ]);
+        $response = $this->client->request('GET', "https://api.themoviedb.org/3/movie/{$id}/reviews", [
+            'query' => [
+                'api_key' => $this->apiKey,
+                'language' => 'fr-FR'
+            ]
+        ]);
 
-    return $response->toArray();
+        return $response->toArray();
     }
 
     public function searchMovies(string $query): array
