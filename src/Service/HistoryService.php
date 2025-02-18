8ac50274d887ce1da3e7fb16932f1c6c8781619e
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-18 14:13:08 +0100

    Add HistoryService for managing user movie history

---
+++ src/Service/HistoryService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\History;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
+
+/*
+
+*/
+
+class HistoryService
+{
+
+    private $em;
+    private $rs;
+
+    public function __construct(EntityManagerInterface $em, RequestStack $rs)
+    {
+        $this->em = $em;
+        $this->rs = $rs;
+    }
+
+
+    // Crée ou récupèrer l'ID de l'utilisateur
+    public function getUuid(Response $response): string
+    {
+        $request = $this->rs->getCurrentRequest();
+        $uuid = $request->cookies->get('user_uuid');
+
+        if (!$uuid) {
+            $uuid = Uuid::v4()->toRfc4122();
+            $cookie = Cookie::create('user_uuid', $uuid, strtotime('+1 year'));
+            $response->headers->setCookie($cookie);
+        }
+
+        return $uuid;
+    }
+
+
+    public function addMovieToHistory(int $movieId, Response $response): void
+    {
+        $uuid = $this->getUuid($response);
+        $historyRepo = $this->em->getRepository(History::class);
+
+        $history = $historyRepo->findOneBy(['uuid' => $uuid]);
+
+        if (!$history) {
+            $history = new History();
+            $history
+                ->setUserId($uuid)
+                ->setMovies([$movieId])
+            ;
+        }
+
+        $history->addMovie($movieId);
+
+        $this->em->persist($history);
+        $this->em->flush();
+    }
+}
