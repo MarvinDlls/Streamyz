97eaab9ce3ea28b49cece1e8aacd383a07749e92
Marvindlls <marvindillenseger@gmail.com>
2025-02-19 15:04:03 +0100

    Add History and Report entities with corresponding repositories and migration

---
+++ src/Entity/Report.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReportRepository::class)]
+class Report
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $user = null;
+
+    #[ORM\Column]
+    private ?int $tmdb = null;
+
+    #[ORM\PrePersist]
+    public function prePersist(): void
+    {
+        $this->created_at = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTmdb(): ?int
+    {
+        return $this->tmdb;
+    }
+
+    public function setTmdb(int $tmdb): static
+    {
+        $this->tmdb = $tmdb;
+
+        return $this;
+    }
+}
